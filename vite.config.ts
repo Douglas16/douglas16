45b0ef9e075e247029927e5c6ef2c9b320dae045
Douglas Desiderio <38764223+Douglas16@users.noreply.github.com>
2026-04-20 16:47:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,14 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: 'Douglas16.github.io',
+    base: '/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
     server: {
@@ -21,5 +21,9 @@ export default defineConfig(({mode}) => {
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
     },
+    build: {
+      // Garante que o build seja gerado na pasta dist para o GitHub Actions encontrar
+      outDir: 'dist',
+    }
   };
 });
